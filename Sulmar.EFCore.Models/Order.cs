b0b4109a026a5fae75d3b9afb37fd81595970cff
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-08-16 09:27:58 +0200

    Dodanie właściwości Customer

--- Sulmar.EFCore.Models/Order.cs
+++ Sulmar.EFCore.Models/Order.cs
@@ -7,6 +7,7 @@ namespace Sulmar.EFCore.Models
     public class Order : BaseEntity
     {
         public DateTime OrderDate { get; set; }
+        public Customer Customer { get; set; }
         public IEnumerable<OrderDetail> Details { get; set; }
         public OrderStatus Status { get; set; }
         public decimal TotalAmount => Details.Sum(d => d.LineAmount);
