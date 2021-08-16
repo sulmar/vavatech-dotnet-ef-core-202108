c633b9326d4bf56f765b32a186ea79bcab417175
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-08-16 18:15:36 +0200

    Refaktoring

--- Vavatech.EFCore.DbRepositories/ModelBuilderExtentions.cs
+++ Vavatech.EFCore.DbRepositories/ModelBuilderExtentions.cs
@@ -8,26 +8,13 @@ namespace Vavatech.EFCore.DbRepositories
 {
     public static class ModelBuilderExtentions
     {
-        public static IEnumerable<IMutableProperty> Properties<T>(this ModelBuilder modelBuilder)
-        {
-            return from e in modelBuilder.Model.GetEntityTypes()
-                              from p in e.GetProperties()
-                              where p.PropertyInfo?.PropertyType == typeof(T)
-                   select p;
+        public static IEnumerable<IMutableProperty> Properties(this ModelBuilder modelBuilder) => modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetProperties());
 
-        }
-
-        public static IEnumerable<IMutableProperty> Properties(this ModelBuilder modelBuilder)
-        {
-            return from e in modelBuilder.Model.GetEntityTypes()
-                   from p in e.GetProperties()
-                   select p;
-
-        }
+        public static IEnumerable<IMutableProperty> Properties<T>(this ModelBuilder modelBuilder) => modelBuilder.Properties().Where(p => p.PropertyInfo?.PropertyType == typeof(T));
 
         public static void Configure(this IEnumerable<IMutableProperty> properties, Action<IMutableProperty> configuration)
         {
-            foreach (var property in properties)
+            foreach (IMutableProperty property in properties)
             {
                 configuration.Invoke(property);
             }
