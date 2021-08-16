50380b0aba1a9338819bf79447cb23f5a3aea874
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-08-16 16:46:22 +0200

    Utworzenie aplikacji webowej MVC

---
+++ Vavatech.EFCore.MVCApp/Controllers/CustomersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vavatech.EFCore.IRepositories;
+
+namespace Vavatech.EFCore.MVCApp.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly ICustomerRepository customerRepository;
+
+        public CustomersController(ICustomerRepository customerRepository)
+        {
+            this.customerRepository = customerRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var customers = customerRepository.Get();
+
+            return View(customers);
+        }
+    }
+}
