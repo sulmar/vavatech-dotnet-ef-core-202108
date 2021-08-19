28dca1f087b9dc0e4353bf43c438fca2b563f717
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-08-19 16:58:05 +0200

    SeedData

--- Vavatech.EFCore.DbRepositories/Migrations/20210819145614_SeedLoyaltyCard.Designer.cs
+++ Vavatech.EFCore.DbRepositories/Migrations/20210819145614_SeedLoyaltyCard.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Vavatech.EFCore.DbRepositories;
 
 namespace Vavatech.EFCore.DbRepositories.Migrations
 {
     [DbContext(typeof(ShopContext))]
-    partial class ShopContextModelSnapshot : ModelSnapshot
+    [Migration("20210819145614_SeedLoyaltyCard")]
+    partial class SeedLoyaltyCard
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
